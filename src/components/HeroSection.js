dcd0d3b61022cb0f2923f6227c1865368350484b
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-05-18 17:25:50 -0500

    Changed font color on main page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="/videos/video1.mp4" autoPlay loop muted/>
+        <video src="/videos/video-2.mp4" autoPlay loop muted/>
         <h2>Hi, my name is</h2>
         <h1>Damaris Poppe.</h1>
         <p>I'm a Computer Science student focusing in designing and building creative things for the web. I'm currently working on different front-end projects.</p>
