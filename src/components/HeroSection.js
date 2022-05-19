649e01bc5cd797c12874f204e32ba7565b0f7a08
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-05-18 20:20:38 -0500

    scroll added needs fix

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,14 +2,17 @@ import React from 'react';
 import '../App.css';
 import {Button} from './Button';
 import './HeroSection.css';
- 
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 
 function HeroSection() {
   return (
     <div className='hero-container' id="hero-section">
         <video src="/videos/video-2.mp4" autoPlay loop muted/>
+        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOutUp" offset={500}>  
         <h2>Hi, my name is</h2>
         <h1>Damaris Poppe.</h1>
+        </ScrollAnimation>
         <p>I'm a Computer Science student focusing in designing and building creative things for the web. I'm currently working on different front-end projects.</p>
         <h2>Welcome to my portfolio!</h2>
         <div container className='hero-btns'>
@@ -20,7 +23,9 @@ function HeroSection() {
             >
                 CONTACT
             </Button>
-        
+        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOutUp" initiallyVisible={true} offset={100} className="scroll">
+            <span>Scroll</span>
+        </ScrollAnimation>
         </div>
     </div>
   )
