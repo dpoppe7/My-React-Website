2267180264440fbd44a29117ee26c298f06781e7
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-05-21 18:37:02 -0500

    Changed button and fonts on mobile size, changed scroll size

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,10 +9,8 @@ function HeroSection() {
   return (
     <div className='hero-container' id="hero-section">
         <video src="/videos/video-2.mp4" autoPlay loop muted/>
-        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOutUp" offset={500}>  
         <h2>Hi, my name is</h2>
         <h1>Damaris Poppe.</h1>
-        </ScrollAnimation>
         <p>I'm a Computer Science student focusing in designing and building creative things for the web. I'm currently working on different front-end projects.</p>
         <h2>Welcome to my portfolio!</h2>
         <div container className='hero-btns'>
